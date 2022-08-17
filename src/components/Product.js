3ac0fa643bed701bff3f282ee8031235eeb4b642
KimHanDeul <dle1883@gmail.com>
2022-08-17 18:51:34 +0900

    useEffect

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
 function Product(props) {
+    let navigate = useNavigate();
     return (
-        <div className="col-md-4">
+        <div className="col-md-4" onClick={ () => {navigate(`/detail/${props.idx}`)} } style={{cursor:'pointer'}}>
             <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} alt="product_img" width='80%' />
             <h4>{props.shoes.title}</h4>
             <p>{props.shoes.price}</p>
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,22 +1,19 @@
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
-
-let Btn = styled.button`
-    background : ${props => props.bg};
-    color : ${props => props.bg == 'orange' ? '#000' : '#fff'};
-    padding : 10px;
-    outline : none;
-    border : none;
-`
-
-
+import { useEffect, useState } from 'react';
 
 function Detail(props) {
+
     let {id} = useParams()
     let findProduct = props.shoes.find( (x) => {return x.id == id})
-    console.log(findProduct)
+    let [num, setNum] = useState('');
+    useEffect(() => {
+        if(isNaN(num) == true) {
+            alert('숫자만 입력해주세요.')
+        }
+    }, [num])
     return (
         <div className="container">
+            <input onChange={ (e) => { setNum(e.target.value) }}></input>
             <div className="row">
                 <div className="col-md-6">
                     <img src={`https://codingapple1.github.io/shop/shoes${Number(id) + 1}.jpg`} width="100%" />
@@ -26,8 +23,6 @@ function Detail(props) {
                     <p>{findProduct.content}</p>
                     <p>{findProduct.price}</p>
                     <button className="btn btn-danger">주문하기</button>
-                    <Btn bg="orange">스타일드 컴포넌트</Btn>
-                    <Btn bg="tomato">스타일드 컴포넌트</Btn>
                 </div>
             </div>
         </div>
