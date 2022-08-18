e04d832b78d0af79077a8b63797329c41162cfd2
KimHanDeul <dle1883@gmail.com>
2022-08-18 16:41:56 +0900

    상품 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import data from './data.js'
 import Detail from './pages/Detail.js'
 import Product from './components/Product.js'
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
     let [shoes, setShoes] = useState(data);
     let navigate = useNavigate();
+    
     return (
         <div className="App">
 
             <Navbar bg="white" variant="white">
                 <Container>
-                    <Navbar.Brand href="#home">Cos Wear</Navbar.Brand>
+                    <Navbar.Brand href="/">Cos Wear</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link onClick={ () => { navigate('/') }}>홈으로</Nav.Link>
                         <Nav.Link onClick={ () => { navigate('/detail') }}>상세 페이지</Nav.Link>
@@ -39,6 +41,15 @@ function App() {
                                 }
                             </div>
                         </div>
+                        <button className="more_view" onClick={ () => {
+                            axios.get('https://codingapple1.github.io/shop/data2.json')
+                            .then( (result) => {
+                                let copy = [...shoes, ...result.data];
+                                setShoes(copy);
+                                console.log(result.data);
+                            })
+                            .catch( () => { alert('sorry no data')})
+                        }}>더 보기</button>
                     </>
                 } />
                 <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
