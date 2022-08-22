acca3bdd74b3e61711a42bafaf6e63621070a661
KimHanDeul <dle1883@gmail.com>
2022-08-22 17:16:40 +0900

    더보기 loading 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import data from './data.js'
 import Detail from './pages/Detail.js'
 import Product from './components/Product.js'
+import Loading from './components/Loading.js'
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import axios from 'axios'
 
 function App() {
     let [shoes, setShoes] = useState(data);
     let navigate = useNavigate();
-    
+    let [countView, setCountView] = useState(1);
+    let [loading, setLoading] = useState(false);
     return (
         <div className="App">
 
@@ -18,14 +20,14 @@ function App() {
                 <Container>
                     <Navbar.Brand href="/">Cos Wear</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link onClick={ () => { navigate('/') }}>홈으로</Nav.Link>
-                        <Nav.Link onClick={ () => { navigate('/detail') }}>상세 페이지</Nav.Link>
+                        <Nav.Link onClick={() => { navigate('/') }}>홈으로</Nav.Link>
+                        <Nav.Link onClick={() => { navigate('/detail') }}>상세 페이지</Nav.Link>
                         {/* <Link to="/">홈으로</Link> */}
                         {/* <Link to="/detail">상세 페이지</Link> */}
                     </Nav>
                 </Container>
             </Navbar>
-            
+
             <Routes>
                 <Route path="/" element={
                     <>
@@ -41,18 +43,27 @@ function App() {
                                 }
                             </div>
                         </div>
-                        <button className="more_view" onClick={ () => {
-                            axios.get('https://codingapple1.github.io/shop/data2.json')
-                            .then( (result) => {
-                                let copy = [...shoes, ...result.data];
-                                setShoes(copy);
-                                console.log(result.data);
-                            })
-                            .catch( () => { alert('sorry no data')})
+                        {loading == true ? <Loading /> : null} 
+                        <button className="more_view" onClick={() => {
+                            setLoading(true);
+                            setCountView(countView + 1);
+                            axios.get(`/data${countView + 1}.json`)
+                                .then((result) => {
+                                    let copy = [...shoes, ...result.data];
+                                    setShoes(copy);
+                                    setLoading(false);
+                                })
+                                .catch(() => { 
+                                    alert('sorry no data');
+                                    setLoading(false); 
+                                })
+                            // Promise.all([ axios.get('/url1') ], [ axios.get('/url2') ])
+                            // .then( () => {})
                         }}>더 보기</button>
+
                     </>
                 } />
-                <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+                <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
                 <Route path="*" element={<div>없는 페이지입니다.</div>} />
             </Routes>
 
