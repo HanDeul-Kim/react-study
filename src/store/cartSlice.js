33f8a6e0081afe3fc97a9c6388cfeed3ac6c09bb
KimHanDeul <dle1883@gmail.com>
2022-09-11 23:51:01 +0900

    중복배열 찾기 오류

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 let cart = createSlice({
     name: 'cart',
-    initialState: 
+    initialState:
         [],
-    reducers : {
+    reducers: {
         addCount(state, action) {
-            let findProduct = state.findIndex( (a) => { return a.id === action.payload })
+            let findProduct = state.findIndex((a) => { return a.id === action.payload })
             state[findProduct].count++
         },
         addProduct(state, action) {
-            state.push(action.payload)
+            // state.push(action.payload)
 
-            state.filter((item, idx, self) => {
-                if(state.includes(item)) {
-                    // state.pop(0)
-                } else{
-                    state.push(action.payload)
-                }
-            })
-            
+
+            let returnValue = state.find((data, idx) => { return data.id === idx });
+            if (!state.includes(returnValue)) {
+                state.push(action.payload)
+            } 
+            console.log(returnValue)
+
         }
     }
 })
-export let {addCount, addProduct} = cart.actions
+export let { addCount, addProduct } = cart.actions
 export default cart
